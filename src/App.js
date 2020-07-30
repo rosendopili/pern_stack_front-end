6e923c760fb7d110289b3e30a65deddbef9d5add
rosendopili <rosendopili@gmail.com>
2020-07-29 21:31:40 -0400

    adding front-end logic

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props); 
+    this.state = {
+      projects: [], 
+      dataLoaded: false
+    }
+  }
+  componentDidMount = () => {
+    fetch("http://localhost:3030/projects")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({
+          projects: res,
+          dataLoaded: true,
+      })
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1> Projects </h1>
+
+
+        {this.state.projects.length > 0 && this.state.projects.reverse().map(project => {
+          return(
+            <div className="projectCard">
+            <h2 className="projectName">{project.name}</h2>
+            <img className="projectImage" src={project.url}/> 
+            <p className="projectDescription">
+              {project.description}
+            </p>
+            </div>
+          )      
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
